f24c8c36b8c5150fd94ebdf725dee19c2a0d1a8f
stein <stein@americamail.com>
2019-02-16 15:34:12 +0100

    cleaned up files

--- gui_test/tweets_analysis_gui.py
+++ gui_test/tweets_analysis_gui.py
@@ -1,27 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb  2 12:40:34 2019
+Created on Sat Feb  2 12::34 2019
 
 @author: stein
 """
 
-#############
-# Libraries
-#############
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
-import warnings
 import json
+from PyQt5 import QtCore, QtGui, QtWidgets
 import re
-from wordcloud import WordCloud, ImageColorGenerator
-import matplotlib.pyplot as plt
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize, TweetTokenizer
 from nltk.corpus import stopwords
-
-# Define UI
-qtCreatorFile = "tweet_text_analysis.ui"
-Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+from wordcloud import WordCloud, ImageColorGenerator
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
 
 #############
 # Classes
@@ -69,33 +63,107 @@ class NormalizeText():
                 pass      
         return (s)
 
-class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        self.fileName = ''
-        QtWidgets.QMainWindow.__init__(self)
-        self.setupUi(self)
+class Window(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+
+        # a figure instance to plot on
+        self.figure = plt.figure()
+
+        # this is the Canvas Widget that displays the `figure`
+        # it takes the `figure` instance as a parameter to __init__
+        self.canvas = FigureCanvas(self.figure)
+
+        # this is the Navigation widget
+        # it takes the Canvas widget and a parent
+        #self.toolbar = NavigationToolbar(self.canvas, self)    #matplot lib toolbar. Not needed for wordcloud
+
+        # Add interface elements
         
+        # Header layout
+        self.bSelectFile = QtWidgets.QPushButton('Select file...')
+        self.lFileName = QtWidgets.QLineEdit()
+        self.lFileName.setEnabled(False)
+
+        self.l1 = QtWidgets.QLabel('Language:')
+        self.cLang = QtWidgets.QComboBox()
+        self.cLang.addItem('English')
+        self.cLang.addItem('Spanish')
+
+        self.bAnalyze = QtWidgets.QPushButton('Analyze')
+
+        headerLayout = QtWidgets.QHBoxLayout()
+        headerLayout.addWidget(self.bSelectFile)
+        headerLayout.addWidget(self.lFileName)
+        headerLayout.addWidget(self.l1)
+        headerLayout.addWidget(self.cLang)
+        headerLayout.addWidget(self.bAnalyze)
+
+
+        # Bottom Layout
+        self.tAnalysis = QtWidgets.QTextBrowser()
+        self.tDetails = QtWidgets.QTextBrowser()
+        # Set textbrowser properties
+        self.tAnalysis.setFontPointSize(10)
+        self.tDetails.setFontPointSize(10)
+
+        bottomLayout = QtWidgets.QHBoxLayout()
+        bottomLayout.addWidget(self.tAnalysis)
+        bottomLayout.addWidget(self.tDetails)
+
+
+        # Connect buttons to methods
+        # self.button = QtWidgets.QPushButton('Plot')
+        # self.button.clicked.connect(self.plot)
         self.bSelectFile.clicked.connect(self.openFilenameDialog)
         self.bAnalyze.clicked.connect(self.tweetAnalyse)
-        self.bClose.clicked.connect(self.closeEvent)
 
-        CLOUD_WORDS = 50    # Number of words to draw in the word cloud
-        READ_SPEED = 3      # 3 words per second
-        
-    def closeEvent(self):
-        # Close the application
-        app.quit()
+        # set the layout
+        layout = QtWidgets.QVBoxLayout()
 
-    def tDetails(self):
-        # Table double click, show the tweet text details
-        # Get the location of the double click
-        for idx in self.tTweets.selectionModel().selectedIndexes():
-            row_number = idx.row()
-            column_number = idx.column()
-        # Get the text details
-        item = self.tTweets.item(row_number, column_number)
-        # Create a pop up window with the details
-        QtWidgets.QMessageBox.about(self, 'Tweet', item.text())
+        layout.addLayout(headerLayout)
+
+        #layout.addWidget(self.toolbar) #matplot lib toolbar. Not needed for wordcloud
+        layout.addWidget(self.canvas)
+        # layout.addWidget(self.button)
+
+        layout.addLayout(bottomLayout)
+
+        self.setLayout(layout)
+
+    def plot(self):
+        ''' plot some random stuff '''
+        # random data
+        data = [random.random() for i in range(10)]
+
+        # discards the old graph
+        self.figure.clear()
+
+        # create an axis
+        ax = self.figure.add_subplot(111)
+
+        # plot data
+        ax.plot(data, '*-')
+
+        # refresh canvas
+        self.canvas.draw()
+
+    def pList(self, list):
+        # Print a formated list of tuples
+        self.items = list
+        for item in self.items:
+            line = '{} {:^2} {}'.format(item[0], '->', item[1])
+            self.tDetails.append(line)
+    
+    def openFilenameDialog(self):
+        options = QtWidgets.QFileDialog.Options()
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        self.fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","JSON files (*.json)", options=options)
+        if self.fileName:
+            self.lFileName.setText(self.fileName)
+            return
+        else:
+            return()
 
     def read_json(self, json_file):
         with open(json_file) as file:
@@ -127,41 +195,29 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
                 pass      
         return (text)
 
-    def openFilenameDialog(self):
-        options = QtWidgets.QFileDialog.Options()
-        options |= QtWidgets.QFileDialog.DontUseNativeDialog
-        self.fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","JSON files (*.json)", options=options)
-        if self.fileName:
-            self.lFileName.setText(self.fileName)
-            return
-        else:
-            return()
-
     def tweetAnalyse(self):
+
+        # Clear text browser boxes
+        self.tAnalysis.clear()
+        self.tDetails.clear()
+
         # Read the tweets file
         if self.fileName == '':
             return
-        self.lStatusline.setText('Reading JSON file...')
+        # self.lStatusline.setText('Reading JSON file...')
         tweets = self.read_json(self.fileName)
-
-        # Clear text browser box
-        self.tAnalysis.clear()
 
         # Define stopwords dictionary
         # Read language combox selection
         lang = str(self.cLang.currentText()).lower()
-        print (lang)
         if lang == 'spanish':
             stop_words = set(stopwords.words('spanish'))
             bad_words = self.load_badwords('es')
-            print ('Using SPANISH stopwords/profanity dictionary')
         else:
             stop_words = set(stopwords.words('english'))
             bad_words = self.load_badwords('en')
-            print ('Using ENGLISH stopwords/profanity dictionary')
 
         # Extract and pre-process tweets text
-        self.lStatusline.setText('Extracting text from tweets...')
         text = ''
         for tweet in tweets:
             if tweet['truncated']:
@@ -178,39 +234,30 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
             # Add line to text body
             text = text + line + '\n'
 
-        self.lStatusline.setText('Pre-processing file...')
         # Remove non-ascii characters
-        self.lStatusline.setText('Removing non-ascii characters...')
         text = NormalizeText.remove_nonascii(text)
         # Replace accent and special character
-        self.lStatusline.setText('Replacing special characters...')
         text = NormalizeText.remove_special(text)
 
         # Create a copy of the text
         text_raw = text
 
         # Eliminate the punctuation signs
-        self.lStatusline.setText('Eliminating punctuation signs...')
         text = NormalizeText.remove_punctuation(text)
         # Eliminate new lines
-        self.lStatusline.setText('Eliminating new lines characters...')
         text = text.replace('\n', ' ')
         # Eliminate 'RT' flags
-        self.lStatusline.setText('Eliminating RT flags...')
         text = NormalizeText.remove_flags(text)
         # Converting to lowercase
-        self.lStatusline.setText('Converting to lowercase...')
         text = NormalizeText.to_lowercase(text)
 
         # Tokenize words 
-        self.lStatusline.setText('Tokenizing tweets...')
         tTokenizer = TweetTokenizer()
         tTokens = tTokenizer.tokenize(text)
         # Tokenize sentences
         sentence_tokens = nltk.sent_tokenize(text_raw)
 
         # Extract @ and hashtags
-        self.lStatusline.setText('Analysing hashtags...')
         tHash = []
         tAt = []
         for item in tTokens:
@@ -232,7 +279,6 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         used_bad_words = [word for word in tTokens if word.lower() in bad_words]
 
         # Calculate frequency distribution
-        self.lStatusline.setText('Calculating frequency distribution...')
         fdist = nltk.FreqDist(tTokens)
         fdist1 = nltk.FreqDist(text_nonstop)
         fdist2 = nltk.FreqDist(text_no_badwords)
@@ -252,56 +298,84 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         adj_vocab_rich = (len(text_vocab)-len(fdist_at)-len(fdist_stopwords)-len(fdist_badwords))/(len(tTokens))*100
 
         # Display text file analysis results
-        line = 'Sentences                   : {}'.format(len(sentence_tokens))
+        
+        line = '{} {}'.format('Sentences:'.ljust(25, ' '), len(sentence_tokens))
         self.tAnalysis.append(line)
-        line = 'Total words                 : {}'.format(len(tTokens))
+
+        line = '{} {}'.format('Total words:'.ljust(25, ' '), len(tTokens))
         self.tAnalysis.append(line)
-        line = 'Unique words                : {}'.format(len(text_vocab))
+
+        line = '{} {}'.format('Unique words:'.ljust(25, ' '), len(text_vocab))
         self.tAnalysis.append(line)
-        line = 'Vocabulary richness         : {:.2f}%'.format(len(text_vocab)/len(tTokens)*100)
+
+        line = '{} {:.2f}'.format('Vocabulary richness:'.ljust(25, ' '), len(text_vocab)/len(tTokens)*100)
         self.tAnalysis.append(line)
-        line = 'Unique stopwords            : {}'.format(len(fdist_stopwords))
+
+        line = '{} {}'.format('Unique stopwords:'.ljust(25, ' '), len(fdist_stopwords))
         self.tAnalysis.append(line)
-        line = 'Unique profanity words      : {}'.format(len(fdist_badwords))
+
+        line = '{} {:.0f}'.format('Unique profanity words:'.ljust(25, ' '), len(fdist_badwords))
         self.tAnalysis.append(line)
-        line = 'Total profanity words       : {}'.format(len(used_bad_words))
+
+        line = '{} {:.0f}'.format('Total profanity words:'.ljust(25, ' '), len(used_bad_words))
         self.tAnalysis.append(line)
-        line = 'Adjusted vocabulary richness: {:.2f}'.format(adj_vocab_rich)
+
+        line = '{} {:.2f}'.format('Adj. vocabulary richness:'.ljust(25, ' '), adj_vocab_rich)
         self.tAnalysis.append(line)
-        line = 'Reading time                : {:.1f} min.'.format(read_time)
+
+        line = '{} {:.1f}'.format('Reading time:'.ljust(25, ' '), read_time)
         self.tAnalysis.append(line)
+
+        # Display most common used words
+        self.tDetails.append('<b>15 most common words:</b>')
+        self.tDetails.append('--------------------------------')
+        self.pList(fdist.most_common(15))
+        self.tDetails.append('\n')
+
+        self.tDetails.append('<b>15 most common nonstop words:</b>')
+        self.tDetails.append('--------------------------------------------')
+        self.pList(fdist1.most_common(15))
+        self.tDetails.append('\n')
+
+        self.tDetails.append('<b>15 most common stop words:</b>')
+        self.tDetails.append('-----------------------------------------')
+        self.pList(fdist_stopwords.most_common(15))
+        self.tDetails.append('\n')
+    
+        self.tDetails.append("<b>Most common #'s:</b>")
+        self.tDetails.append('-----------------------')
+        self.pList(fdist_hash.most_common(5))
+        self.tDetails.append('\n')
+        
+        self.tDetails.append("<b>Most common @'s:</b>")
+        self.tDetails.append('-----------------------')
+        self.pList(fdist_at.most_common(15))
+        self.tDetails.append('\n')
+
+        # Set focus to top of the text box
+        self.tDetails.verticalScrollBar().setValue(0)
+
+        # Plot the word cloud
+
+        # discards the old graph
+        self.figure.clear()
+
+        # create an axis
+        ax = self.figure.add_subplot(111)
+        self.figure.subplots_adjust(bottom=0, top=1, left=0, right=1)
 
         # Generate word cloud
-        # fig = plt.figure()
-        # ax1 = fig.add_subplot(211)
         wordcloud = WordCloud(stopwords=stop_words, max_words=50, background_color='white').generate(text)
-        self.gCloud.plot(wordcloud)
-        
+        ax.imshow(wordcloud, interpolation='bilinear')
+        ax.axis('off')
 
+        # refresh canvas
+        self.canvas.draw()
 
-
-
-        # Display text file analysis
-# print ('\n****** Analysis Results *******')
-# print ('Sentences                   : {}'.format(len(sentence_tokens)))
-# print ('Total words                 : {}'.format(len(tTokens)))
-# print ('Unique words                : {}'.format(len(text_vocab)))
-# print ('Vocabulary richness         : {:.2f}%'.format(len(text_vocab)/len(tTokens)*100))
-# print ('Unique stopwords            : {}'.format(len(fdist_stopwords)))
-# print ('Total stopwords             : {}'.format(len(used_stopwords)))
-# print ('Unique profanity words      : {}'.format(len(fdist_badwords)))
-# print ('Total profanity words       : {}'.format(len(used_bad_words)))
-# print ('Adjusted vocabulary richness: {:.2f}'.format(adj_vocab_rich))
-# print ('Reading time                : {:.1f} min.'.format(read_time))
-
-        
-
-#############
-# Main Loop
-#############
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    window = MyApp()
-    window.show()
+
+    main = Window()
+    main.show()
+
     sys.exit(app.exec_())
